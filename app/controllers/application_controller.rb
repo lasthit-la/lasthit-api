bd64b5295c08d50cbecdd6706149cdf949bd6497
Diego Flores <diegoflores1991@gmail.com>
2016-02-03 01:11:35 -0300

    [#Gitignore]  - Added gitignore

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
